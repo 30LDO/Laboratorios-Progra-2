f7837d9d0165541e1622e732aa1c94d70aea259d
30LDO <diego.orozco30@gmail.com>
2024-10-30 20:31:15 -0600

    Update lab2.js

--- labs/lab2.js
+++ labs/lab2.js
@@ -99,5 +99,3 @@ console.log(`${student1.fullName}: ${student1.courses.length} courses`); // -> R
 console.log(`${teacher1.fullName}: ${teacher1.courses.length} courses`); // -> Paula Thompkins: 2 courses
 student1.fullName = 'Rafael Fifer';
 console.log(`${student1.fullName}: ${student1.courses.length} courses`); // -> Rafael Fifer: 1 courses
-
-console.log(teacher1)
